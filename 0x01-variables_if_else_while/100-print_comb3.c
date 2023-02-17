e44c2e26b8abfeaa1634e93ed56b89da0c7d5c2a
Soetan Bolaji Micheal <soetanbolaji@gmail.com>
2023-02-17 11:33:34 -0800

    100 teleapha

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,19 +8,37 @@
  */
 int main(void)
 {
-	int d;
+	int i = '0';
+	int j = '0';
 
-	for (d = 0; d < 100; d++)
+	while (i <= '9')
 	{
-		putchar((d / 10) + '0');
-		putchar((d % 10) + '0');
-		if (d != 99)
+		while (j <= '9')
 		{
-			putchar(',');
-			putchar(' ');
+			if (!((j * 10 + i) < (i * 10 + j)))
+			{
+				putchar(i);
+				putchar(j);
+
+				if (j == '9' && i == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+			j++;
 		}
+		if (j >= '9')
+		{
+			j = '0';
+		}
+		i++;
 	}
-	putchar('\n');
 
 	return (0);
 }
