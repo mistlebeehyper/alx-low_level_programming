8e3b9243bc473108b1280f08008469a3bb59483c
Soetan Bolaji Micheal <soetanbolaji@gmail.com>
2023-03-07 10:40:44 -0800

    commit 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * wildcmp - Entry Point
- * @s1: input
- * @s2: input
- * Return: 0
+ * wildcmp - compares two string
+ * @s1: string1
+ * @s2: string2
+ * Return: 1 if equal 0 otherwise
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (0);
+	if (*s1 != *s2)
+		return (0);
+	else if (*s1 == 0 && *s2 == 0)
+		return (1);
+	else if (*s2 == '*')
+		wildcmp(s1 + 1, s2);
+	else if (*s2 == '*' && *s1 == *(s2 + 1))
+		return (wildcmp(s1 +1, s2 + 1));
+	return (wildcmp(s1 + 1, s2 + 1));
+
 }
