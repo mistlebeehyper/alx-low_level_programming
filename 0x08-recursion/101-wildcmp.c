1ee4fde7f9748ee50897eee8f541c8d9a6983b75
Soetan Bolaji Micheal <soetanbolaji@gmail.com>
2023-03-10 13:48:14 -0800

    commit 0

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,6 +15,6 @@ int wildcmp(char *s1, char *s2)
 		wildcmp(s1 + 1, s2);
 	else if (*s2 == '*' && *s1 == *(s2 + 1))
 		return (wildcmp(s1 +1, s2 + 1));
-	return (wildcmp(s1 + 1, s2 + 1));
+	return (wildcmp(s1 +1, s2+1));
 
 }
