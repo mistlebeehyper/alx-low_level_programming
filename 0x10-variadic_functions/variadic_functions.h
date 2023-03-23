5801a02558263c4f46ada429e2f2194ca9ee6fbb
Soetan Bolaji Micheal <soetanbolaji@gmail.com>
2023-03-23 13:53:30 -0700

    Commit 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,18 +1,11 @@
-#ifndef VARIADIC_H
-#define VARIADIC_H
-
-#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
-
-typedef struct validTypes
-{
-	char *valid;
-	void (*f)();
-} v_types;
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 
 #endif
